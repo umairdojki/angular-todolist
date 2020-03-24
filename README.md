9a900fdbcefca512f7008c6f2fbcb39f13a8df8c
umairdojki <udojki@gmail.com>
2020-03-24 23:37:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-eusbn5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-eusbn5)
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { ListManagerComponent } from './list-manager/list-manager.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputButtonUnitComponent,
+    TodoItemComponent,
+    ListManagerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
